5e16da945e0829dcc6839a7a8f4a8b2c7e28b20a
Lei Ding <leiding@lei-mbp.home>
2017-01-23 17:53:45 -0500

    Initial Commit

---
+++ helloagain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  helloagain
+//
+//  Created by Lei Ding on 1/23/17.
+//  Copyright © 2017 Lei Ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
